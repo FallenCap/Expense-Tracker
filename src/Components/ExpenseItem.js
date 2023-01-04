f7b4bb2a3121e0cbc735e97d138cbfa5da329f80
FallenCap <fallencap18@gmail.com>
2023-01-05 00:04:32 +0530

    1st commit

---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,16 @@
+import './css/ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
+
+const ExpenseItem = (props) => {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item_description">
+        <h2>{props.title}</h2>
+      </div>
+      <div className="expense-item_price">${props.amount}</div>
+    </Card>
+  );
+};
+export default ExpenseItem;
